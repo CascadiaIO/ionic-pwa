90dddea0be43bb386d53f6fd9eeeb51b04dbe4a0
Jason Bettineski <jason@cascadiaio.com>
2021-09-11 13:25:39 -0800

    Init Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    HttpClientModule,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/dot-notation */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  // 2 api calls. so we can demo a couple methods for caching
+  users = [];
+  joke = null;
+  appIsOnline = true;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  async ngOnInit() {
+    const status = await Network.getStatus();
+    this.appIsOnline = status.connected;
+
+    Network.addListener('networkStatusChange', (statusChange) => {
+      // console.log('Network status changed', statusChange);
+      this.appIsOnline = statusChange.connected;
+    });
+  }
+
+  getData() {
+    this.http.get('https://randomuser.me/api/?results=5').subscribe((res) => {
+      // console.log(`users res ${JSON.stringify(res)}`); //TODO: Remove comment
+      this.users = res['results'];
+    });
+  }
+
+  getOnlineData() {
+    this.http
+      .get('https://api.chucknorris.io/jokes/random')
+      .subscribe((res) => {
+        // console.log(`joke res ${JSON.stringify(res)}`); //TODO: Remove comment
+        this.joke = res;
+      });
+  }
 }
